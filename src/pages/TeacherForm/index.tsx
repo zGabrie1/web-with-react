6f913b7c4d8f46702e92b41e6188abb0706a24ed
zGabrie1 <gabriel980500@gmail.com>
2020-09-03 10:41:19 -0300

    StyleTeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
 import './style.css';
+import warningIcon from '../../assets/images/icons/warning.svg'
 import Input from '../../components/Input';
 
 function TeacherForm() {
@@ -22,6 +23,25 @@ function TeacherForm() {
                     <Input name="whatsApp" label="WhatsApp"/>
 
                 </fieldset>
+                
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="cost" label="Custo por aula"/>
+
+                </fieldset>
+
+                <footer>
+                 <p>
+                    <img src= { warningIcon } alt="Aviso importante"/> 
+                    Importante! <br/>
+                    Preencha todos os dados
+                 </p>
+                 <button type="button">
+                     Salvar cadastro
+                 </button>
+                </footer>
             </main>
 
         </div>
